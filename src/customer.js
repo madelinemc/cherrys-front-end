f8e839e3a353e45273750a24bef2ee4663a80f93
madelinemc <madeline.coons@gmail.com>
2021-01-28 17:17:36 -0600

    added customer class and got fetch request to persist new customer to db working

---
+++ src/customer.js
@@ -0,0 +1,12 @@
+class Customer {
+    constructor(id, name, phoneNumber){
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    static test(){
+        console.log("if you can see this, appliance class is working")
+    }
+    
+}
--- src/index.js
+++ src/index.js
@@ -6,26 +6,31 @@ const createBr = document.createElement("br");
 //add event listener to newCustomerForm submit
 //with inline callback function of fetch request to post to /customers
 
-const getStartedButton = document.getElementsByTagName("button")[0];
+const showCustomerForm = true //to prevent making multiple
+const getStartedButton = document.getElementById("get-started-button");
 getStartedButton.addEventListener("click", makeNewCustomerForm);
 
 function makeNewCustomerForm() {
+
     let newCustomerForm = document.createElement("form");
     newCustomerForm.setAttribute("method", "post");
     newCustomerForm.setAttribute("action", "submit");
 
     let newCustomerName = document.createElement("input");
     newCustomerName.setAttribute("type", "text");
+    newCustomerName.setAttribute("id", "new-customer-name");
     newCustomerName.setAttribute("name", "name");
     newCustomerName.setAttribute("placeholder", "enter your name");
 
     let newCustomerPhoneNumber = document.createElement("input");
     newCustomerPhoneNumber.setAttribute("type", "text");
+    newCustomerPhoneNumber.setAttribute("id", "new-customer-phone");
     newCustomerPhoneNumber.setAttribute("name", "phone_number");
     newCustomerPhoneNumber.setAttribute("placeholder", "enter your phone number");
 
     let newCustomerSubmit = document.createElement("input");
     newCustomerSubmit.setAttribute("type", "submit");
+    newCustomerSubmit.setAttribute("id", "new-customer-submit");
     newCustomerSubmit.setAttribute("value", "submit");
 
         newCustomerForm.appendChild(newCustomerName);
@@ -38,15 +43,47 @@ function makeNewCustomerForm() {
 
         document.getElementsByClassName("grid center")[0]
     .appendChild(newCustomerForm);
+
+    newCustomerSubmitButton = document.getElementById("new-customer-submit");
+    newCustomerSubmitButton.addEventListener("click", fetchMakeNewCustomer);
+
 }
-const header = document.getElementsByClassName('grid header')
 
 
-// fetch("http://localhost:3000/customers")
-//     .then(function(response){
-//         return response.json()
-//     })
-//     .then(function(customerObj){
-//         header.innerHTML += `
-//         <h1>${customerObj.name} screams for ice cream!</h1>`
-//     })
+const header = document.getElementsByClassName("grid header")[0];
+
+function fetchMakeNewCustomer(e) {
+    e.preventDefault(); //dont go to url do code below first
+
+    let newCustomerNameValue = document.getElementById("new-customer-name").value
+    let newCustomerPhoneValue = document.getElementById("new-customer-phone").value
+
+   fetch("http://localhost:3000/customers", {
+       method: "POST",
+       headers: { 
+           "Content-Type": "application/json",
+           "Accept": "application/json"
+        },
+       body: JSON.stringify({
+           name: newCustomerNameValue,
+           phone_number: newCustomerPhoneValue,
+       })
+    })
+
+    .then(resp=> resp.json())
+    .then(function(json){
+        console.log(json)
+        customer = new Customer(json.id, json.name, json.phone_number)
+        //iterate over if collection
+        //this is the point where you can store the user id in data-id=${customer.id}
+    })
+            // header.innerHTML += 
+            //`<h1>${customerObj.name} screams for ice cream!</h1>`
+        // .catch(function(error) {
+        //     alert("there were errors"); //come back to this
+        // })
+
+
+
+
+}
