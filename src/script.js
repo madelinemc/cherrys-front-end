1efcdfbfdcedca16f158ee7771635c83b97a4087
madelinemc <madeline.coons@gmail.com>
2021-01-31 21:19:05 -0600

    added drap and drop functionality

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.dataTransfer.dropEffect = "copy"; //relook at this
+}
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    const id = event.dataTransfer.getData('text')
+
+    const draggableElement = document.getElementById(id)
+    const dropzone = event.target
+    dropzone.appendChild(draggableElement)
+
+    event.dataTransfer.clearData();
+}
